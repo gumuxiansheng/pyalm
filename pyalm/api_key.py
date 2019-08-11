89180960c809e9d2c6856795cdd512ee79c34c99
Mike Zhu <zhuyuan2706@gmail.com>
2019-08-11 11:17:35 +0800

    Update

--- pyalm/api_key.py
+++ pyalm/api_key.py
@@ -3,6 +3,6 @@
 # For other instances you will need to contact the manager of that instance.
 import os
 
-API_KEY = { 'plos'       : 'YOUR API KEY GOES HERE',
+API_KEY = { 'plos'       : '8-z-CESRyssvWe3y1GLX',
             'ojs'        : 'YOUR API KEY GOES HERE',
             'copernicus' : 'YOUR API KEY GOES HERE'}
